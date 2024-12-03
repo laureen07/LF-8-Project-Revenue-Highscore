b06a098334ad02262d02122fdb8d4d051c953cd1
Simon Hellmann <sh@p-kg.de>
2024-12-03 14:20:51 +0100

    work in progress

--- MBranch.py
+++ MBranch.py
@@ -1,41 +1,48 @@
-import csv
+from MCSV import CSV
 
-class MCSV:
+url_a = 'Filiale_A.csv'
+url_b = 'Filiale_B.csv'
+url_c = 'Filiale_C.csv'
+
+class Branch:
     def __init__(self):
-        self.csv_data = None
-        self.data_url = None
-        self.list_days = None
-        self.list_revenue = None
+        self.day = ''
+        self.revenue = 0.0
+        self.revenue_url = ''
 
-    def config_list_revenue(self):
-        cur_list = []
-        for value in self.list_revenue:
-            cur_list.append(float((value.replace('.', '').replace(',', '.').replace('€', ''))))
-        self.list_revenue = cur_list
+list_store_a = []
+list_store_b = []
+list_store_c = []
 
-    def read_csv(self, url):
-        with open(url) as file:
-            _csvReader = csv.reader(file, delimiter=';')
-            temp_list = list(_csvReader)
-            self.list_days = temp_list[0]
-            self.list_revenue = temp_list[1]
+def config_list_revenue(revenue):
+    cur_list = []
+    for value in revenue:
+        cur_list.append(float((value.replace('.', '').replace(',', '.').replace('€', ''))))
+    return cur_list
 
-    def get_branch_data(self, url):
-        self.read_csv(url)
-        self.config_list_revenue()
-class MBranch:
-    def __init__(self):
-        self.day = None
-        self.revenue = None
+def init_stores():
+    csv = CSV()
 
-branch_a = MCSV()
-branch_a.data_url = 'Filiale_A.csv'
-branch_a.get_branch_data(branch_a.data_url)
+    store_a = Branch()
+    store_a.revenue_url = url_a
+    store_b = Branch()
+    store_b.revenue_url = url_b
+    store_c = Branch()
+    store_c.revenue_url = url_c
+    stores = [store_a, store_b, store_c]
 
-branch_b = MCSV()
-branch_b.data_url = 'Filiale_B.csv'
-branch_b.get_branch_data(branch_b.data_url)
-
-branch_c = MCSV()
-branch_c.data_url = 'Filiale_C.csv'
-branch_c.get_branch_data(branch_c.data_url)
+    for store in stores:
+        data = csv.read_csv(store.revenue_url)
+        cur_revenues = data[1]
+        cur_revenues = config_list_revenue(cur_revenues)
+        cur_days = data[0]
+        for i in range(len(cur_revenues)):
+            cur_branch = Branch()
+            cur_branch.revenue = cur_revenues[i]
+            cur_branch.day = cur_days[i]
+            if store.revenue_url == url_a:
+                list_store_a.append(cur_branch)
+            elif store.revenue_url == url_b:
+                list_store_b.append(cur_branch)
+            elif store.revenue_url == url_c:
+                list_store_c.append(cur_branch)
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from MBranch import MCSV
+import MBranch
+
+MBranch.init_stores()
+for day in MBranch.list_store_a:
+    print(day.day)
+    print(day.revenue)
 
 
 
